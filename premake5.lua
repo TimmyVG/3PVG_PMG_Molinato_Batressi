91f649af38f2f9ff610012c6c6c7f1baffeff9a5
batressi <batressi@esat-alumni.com>
2024-09-25 20:03:38 +0200

    premake lua initial commit

--- premake5.lua
+++ premake5.lua
@@ -20,3 +20,96 @@ cfg["exelinkflags"] = conan_exelinkflags
 cfg["frameworks"] = conan_frameworks
 
 end
+
+function conan_config_exec()
+
+    configs = {'Debug','Release','RelWithDebInfo'}
+    for i = 1,3 do
+    
+        local cfg = conan[configs[i]]
+        filter("configurations:"..configs[i])
+            linkoptions { cfg["exelinkflags"] }
+            includedirs{ cfg["includedirs"] }
+            libdirs{ cfg["libdirs"] }
+            links{ cfg["libs"] }
+            links{ cfg["system_libs"] }
+            links{ cfg["frameworks"] }
+            defines{ cfg["defines"] }
+        
+        filter{}
+    
+    end
+    
+end
+
+function conan_config_lib()
+
+    configs = {'Debug','Release','RelWithDebInfo'}
+        for i = 1,3 do
+        
+            local cfg = conan[configs[i]]
+            filter("configurations:"..configs[i])
+            
+            linkoptions { cfg["sharedlinkflags"] }
+                includedirs{ cfg["includedirs"] }
+                defines{ cfg["defines"] }
+            
+            filter{}
+        
+        end
+    
+    end
+
+workspace "Motor"
+
+    configurations { "Debug", "Release", "RelWithDebInfo" }
+    architecture "x64"
+    location "build"
+    cppdialect "c++20"
+    startproject "Window"
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+        runtime "Debug"
+    
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+        runtime "Release"
+    
+    filter "configurations:RelWithDebInfo"
+    defines { "NDEBUG" }
+        optimize "On"
+        runtime "Release"
+        symbols "On"
+    filter {}
+
+project "Motor"
+    kind "StaticLib"
+    targetdir "build/%{cfg.buildcfg}"
+    includedirs "include"
+    conan_config_lib()
+    pchheader "stdafx.hpp"
+    pchsource "src/stdafx.cpp"
+    forceincludes { "stdafx.hpp" }
+
+    files {
+        "premake5.lua",
+        "src/build/conanfile.txt",
+        "src/build/conan.lua",
+        "src/stdafx.cpp", "src/stdafx.hpp",
+        "src/Window.cpp", "include/tija/Window.hpp",
+        }
+
+project"Window"
+    kind "WindowedApp"
+    language "C++"
+    targetdir "build/%{prj.name}/%{cfg.buildcfg}"
+    includedirs "include"
+    links "Tija"
+    conan_config_exec("Debug")
+    conan_config_exec("Release")
+    conan_config_exec("RelWithDebInfo")
+    debugargs { _MAIN_SCRIPT_DIR .. "/examples/data" }
+    files "examples/window.cpp"
